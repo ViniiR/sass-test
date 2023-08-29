eda29d2bc102dae7bd5be6b090842017bc92de0b
ViniiR <cvrm2004@gmail.com>
2023-08-29 14:10:56 -0300

    first and second articles done

--- dist/mobile.c1aa418c.js
+++ dist/mobile.c1aa418c.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64916" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52222" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
