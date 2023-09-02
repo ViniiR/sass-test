7184181a7f6bce66b592ee7b5bc3da0b9d4d70f2
ViniiR <cvrm2004@gmail.com>
2023-09-01 23:31:21 -0300

    finishing footer

--- dist/mobile.c1aa418c.js
+++ dist/mobile.c1aa418c.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55571" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64423" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
